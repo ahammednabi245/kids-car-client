8d10e4226d8ba5d7b6083380c4a3e9eb9550e50b
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-05-21 07:53:37 +0600

    OFFER

--- src/components/MyToys/MyToys.jsx
+++ src/components/MyToys/MyToys.jsx
@@ -2,13 +2,12 @@ import React, { useContext, useEffect, useState } from 'react';
 import useTitle from '../../hooks/useTitle';
 import { AuthContext } from '../../providers/AuthProvider';
 import { Link } from 'react-router-dom';
-
+import Swal from 'sweetalert2';
 
 const MyToys = () => {
     useTitle('My Toys');
     const { user } = useContext(AuthContext);
     const [myToys, setMyToys] = useState([]);
-
 
     useEffect(() => {
         const fetchMyToys = async () => {
@@ -24,6 +23,37 @@ const MyToys = () => {
         fetchMyToys();
     }, [user]);
 
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/myToys/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Coffee has been deleted.',
+                                'success'
+                            );
+                            const remaining = myToys.filter(toy => toy._id !== _id);
+                            setMyToys(remaining);
+                        }
+                    });
+            }
+        });
+    };
 
     return (
         <div>
@@ -49,14 +79,14 @@ const MyToys = () => {
                                 <td>{myToy.price}</td>
                                 <td>{myToy.quantity}</td>
                                 <td>
-                                    <Link to={`/updateToy/${myToy._id}`}> <button
-                                        className="btn bg-[#0cabfb] border-none my-2 text-white hover:bg-[#3ddbff]">
-                                        Update
-                                    </button>
+                                    <Link to={`/updateToy/${myToy._id}`}>
+                                        <button className="btn bg-[#0cabfb] border-none my-2 text-white hover:bg-[#3ddbff]">
+                                            Update
+                                        </button>
                                     </Link>
                                 </td>
                                 <td>
-                                    <button className="btn bg-[#0cabfb] border-none my-2 text-white hover:bg-[#3ddbff]">
+                                    <button onClick={() => handleDelete(myToy._id)} className="btn bg-[#0cabfb] border-none my-2 text-white hover:bg-[#3ddbff]">
                                         Delete
                                     </button>
                                 </td>
