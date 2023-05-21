61e68318d77b2f61adc1f3365a25dc31bbccaf7a
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-05-21 18:09:02 +0600

    Add Update My Toys page

--- src/components/MyToys/UpdateMyToys.jsx
+++ src/components/MyToys/UpdateMyToys.jsx
@@ -62,8 +62,10 @@ const UpdateMyToys = () => {
 
     return (
         <div>
-            <div className="bg-[#f2faff] p-24">
-                <h2 className="text-5xl text-center text-slate-500 font-bold mb-8 ">Update  Toy</h2>
+            <div className=" p-24">
+                <div className='w-80 mx-40 '>
+                    <h2 className="text-5xl  text-center border-[#0cabfb] p-3  border rounded text-slate-500 font-bold mb-8 ">Update Toy</h2>
+                </div>
                 <form onSubmit={handleToyUpdate}>
 
 
@@ -72,16 +74,16 @@ const UpdateMyToys = () => {
                             <label className="label">
                                 <span className="label-text">Toy Name</span>
                             </label>
-                            <label className="input-group">
-                                <input type="text" name="name" defaultValue={name} placeholder="Toy Name" className="input input-bordered w-full" />
+                            <label className="">
+                                <input type="text" name="name" defaultValue={name} placeholder="Toy Name" className="input input-bordered border-[#0cabfb] w-full" />
                             </label>
                         </div>
                         <div className="form-control md:w-1/2 ml-4">
                             <label className="label">
                                 <span className="label-text">Available Quantity</span>
                             </label>
-                            <label className="input-group">
-                                <input type="text" name="quantity" defaultValue={quantity} placeholder="Available Quantity" className="input input-bordered w-full" />
+                            <label className="">
+                                <input type="text" name="quantity" defaultValue={quantity} placeholder="Available Quantity" className="input input-bordered border-[#0cabfb] w-full" />
                             </label>
                         </div>
 
@@ -91,8 +93,8 @@ const UpdateMyToys = () => {
                             <label className="label">
                                 <span className="label-text">Seller Name</span>
                             </label>
-                            <label className="input-group">
-                                <input type="text" name="sellerName" defaultValue={sellerName} placeholder="Seller Name" className="input input-bordered w-full" />
+                            <label className="">
+                                <input type="text" name="sellerName" defaultValue={sellerName} placeholder="Seller Name" className="input input-bordered border-[#0cabfb] w-full" />
                             </label>
                         </div>
 
@@ -100,8 +102,8 @@ const UpdateMyToys = () => {
                             <label className="label">
                                 <span className="label-text">Seller Email</span>
                             </label>
-                            <label className="input-group">
-                                <input type="email" name="sellerEmail" defaultValue={sellerEmail} placeholder="Seller Email" className="input input-bordered w-full" />
+                            <label className="">
+                                <input type="email" name="sellerEmail" defaultValue={sellerEmail} placeholder="Seller Email" className="input input-bordered border-[#0cabfb] w-full" />
                             </label>
                         </div>
 
@@ -110,16 +112,16 @@ const UpdateMyToys = () => {
                             <label className="label">
                                 <span className="label-text">Price</span>
                             </label>
-                            <label className="input-group">
-                                <input type="text" name="price" defaultValue={price} placeholder="Price" className="input input-bordered w-full" />
+                            <label className="">
+                                <input type="text" name="price" defaultValue={price} placeholder="Price" className="input input-bordered border-[#0cabfb] w-full" />
                             </label>
                         </div>
                         <div className="form-control md:w-1/2 ml-4">
                             <label className="label">
                                 <span className="label-text">Rating</span>
                             </label>
-                            <label className="input-group">
-                                <input type="text" name="rating" defaultValue={rating} placeholder="Rating" className="input input-bordered w-full" />
+                            <label className="">
+                                <input type="text" name="rating" defaultValue={rating} placeholder="Rating" className="input input-bordered border-[#0cabfb] w-full" />
                             </label>
                         </div>
 
@@ -129,16 +131,16 @@ const UpdateMyToys = () => {
                             <label className="label">
                                 <span className="label-text">Sub-Category</span>
                             </label>
-                            <label className="input-group">
-                                <input type="text" name="subCategory" defaultValue={subCategory} placeholder="Sub-Category" className="input input-bordered w-full" />
+                            <label className="">
+                                <input type="text" name="subCategory" defaultValue={subCategory} placeholder="Sub-Category" className="input input-bordered border-[#0cabfb] w-full" />
                             </label>
                         </div>
                         <div className="form-control md:w-1/2 ml-4">
                             <label className="label">
                                 <span className="label-text"> Description</span>
                             </label>
-                            <label className="input-group">
-                                <input type="text" name="description" defaultValue={description} placeholder="Description" className="input input-bordered w-full" />
+                            <label className="">
+                                <input type="text" name="description" defaultValue={description} placeholder="Description" className="input input-bordered border-[#0cabfb] w-full" />
                             </label>
                         </div>
 
@@ -148,13 +150,15 @@ const UpdateMyToys = () => {
                                 <label className="label">
                                     <span className="label-text">Photo URL of the toy</span>
                                 </label>
-                                <label className="input-group">
-                                    <input type="text" name="photo" defaultValue={photo} placeholder="Photo URL of the toy" className="input input-bordered w-full" />
+                                <label className="">
+                                    <input type="text" name="photo" defaultValue={photo} placeholder="Photo URL of the toy" className="input input-bordered border-[#0cabfb] w-full" />
                                 </label>
                             </div>
                         </div>
-                        <input type="submit" value="Update Toy" className="btn btn-wide bg-[#0cabfb] border-none my-2 ml-4 text-white
+                        <div className='mx-40'>
+                            <input type="submit" value="Update Toy" className="btn btn-wide bg-[#0cabfb] border-none my-2 ml-4 text-white
                          hover:bg-[#3ddbff]" />
+                        </div>
                     </div>
 
                 </form>
