0ee5e112dd70e073653b1648a08df414cf20d809
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-05-21 08:48:17 +0600

    Add a Toy page

--- src/components/AllToys/AllToys.jsx
+++ src/components/AllToys/AllToys.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import useTitle from '../../hooks/useTitle';
 import AllToysTables from './AllToysTables';
+import Swal from 'sweetalert2';
 
 const AllToys = () => {
     useTitle('All Toys');
@@ -8,7 +9,7 @@ const AllToys = () => {
     const [toys, setToys] = useState([]);
     const [showAllToys, setShowAllToys] = useState(false);
     const [page, setPage] = useState(1);
-    const [searchText, setSearchText] = useState("");
+    const [searchText, setSearchText] = useState('');
 
     useEffect(() => {
         fetch(`http://localhost:5000/allToys?page=${page}&limit=20`)
@@ -21,16 +22,41 @@ const AllToys = () => {
             });
     }, [page]);
 
-
     const handleSearch = () => {
+        if (searchText.trim() === '') {
+            let timerInterval
+            Swal.fire({
+                title: 'Please write what you are looking for!',
+                html: 'I will close in <b></b> milliseconds.',
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                  Swal.showLoading()
+                  const b = Swal.getHtmlContainer().querySelector('b')
+                  timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                  }, 100)
+                },
+                willClose: () => {
+                  clearInterval(timerInterval)
+                }
+              }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                  console.log('I was closed by the timer')
+                }
+              })
+            return;
+        }
+
         fetch(`http://localhost:5000/allToysByText/${searchText}`)
             .then((res) => res.json())
             .then((data) => {
                 console.log(data);
                 setToys(data);
+                setSearchText(''); // Reset the search field
             });
     };
-
 
     const handleClickShowMore = () => {
         setPage((prevPage) => prevPage + 1);
@@ -44,8 +70,17 @@ const AllToys = () => {
             <div className='flex justify-center'>
                 <div className="form-control my-6 ">
                     <div className="input-group">
-                        <input onChange={(e) => setSearchText(e.target.value)} type="text" placeholder="Search…" className="input  input-bordered" />
-                        <button onClick={handleSearch} className="btn btn-square bg-[#0cabfb] border-none  text-white hover:bg-[#3ddbff]">
+                        <input
+                            onChange={(e) => setSearchText(e.target.value)}
+                            type="text"
+                            placeholder="Search…"
+                            className="input input-bordered"
+                            value={searchText}
+                        />
+                        <button
+                            onClick={handleSearch}
+                            className="btn btn-square bg-[#0cabfb] border-none text-white hover:bg-[#3ddbff]"
+                        >
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                         </button>
                     </div>
